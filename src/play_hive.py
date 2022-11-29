c3aa805f7c2450822c69c3fff5d820edb13f86d7
William Reames <william.reames.19@cnu.edu>
2022-11-28 20:55:06 -0500

    Player can specify custom depth for the AI

--- src/play_hive.py
+++ src/play_hive.py
@@ -7,6 +7,9 @@ import src.agents as agents
 import src.utils as utils
 
 
+custom_depth = None
+
+
 def set_player_arguments():
     arguments = parse_arguments()
 
@@ -15,15 +18,19 @@ def set_player_arguments():
     if arguments['size']:
         # TODO: Add a way to change board size
         pass
+    if arguments['depth']:
+        pass
+
+    return arguments
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "-l",
-        "--load",
-        help="Load a previously saved game from the `data` folder"
+        '-l',
+        '--load',
+        help='Load a previously saved game from the `data` folder'
     )
     parser.add_argument(
         "-s",
@@ -32,10 +39,22 @@ def parse_arguments():
         type=int,
         default=12
     )
+    parser.add_argument(
+        "-d",
+        "--depth",
+        help=
+        """
+        Enter the maximum depth the AI can search as an integer value. This is equivalent to how many turns in
+        the future the AI will check. For reference, the easy AI looks 0 moves in the future, the medium AI
+        looks 1 move in the future, and the hard AI looks 2 moves in the future. It should be noted, though, that
+        searching a single extra turn in the future increases the time the AI takes to think exponentially.
+        """,
+        type=int
+    )
 
     args = parser.parse_args()
 
-    return {'load': args.load, 'size': args.size}
+    return {'load': args.load, 'size': args.size, 'depth': args.depth}
 
 
 def play_game(player1, player2):
@@ -71,7 +90,7 @@ def play_game(player1, player2):
 
 
 if __name__ == '__main__':
-    set_player_arguments()
+    arguments = set_player_arguments()
     player = agents.HexPlayer()
 
     play_against_player = 'Another player'
@@ -79,42 +98,51 @@ if __name__ == '__main__':
     play_against_easy = 'An easy AI'
     play_against_medium = 'A medium AI'
     play_against_hard = 'A hard AI'
-    opponent_selection = utils.make_choice(
-        'Choose your opponent.',
-        'Select from the following options:',
-        [
-            play_against_player,
-            play_against_random,
-            play_against_easy,
-            play_against_medium,
-            play_against_hard,
-        ]
-    )
 
+    if not arguments['depth']:
+        opponent_selection = utils.make_choice(
+            'Choose your opponent.',
+            'Select from the following options:',
+            [
+                play_against_player,
+                play_against_random,
+                play_against_easy,
+                play_against_medium,
+                play_against_hard,
+            ]
+        )
+        print()
+    else:
+        opponent_selection = arguments['depth']
+
+    if opponent_selection == play_against_player:
+        opponent = agents.HexPlayer()
     if opponent_selection == play_against_random:
         opponent = agents.RandomActionAI()
     elif opponent_selection == play_against_easy:
         opponent = agents.BestNextMoveAI()
     elif opponent_selection == play_against_medium:
         opponent = agents.MinimaxAI(max_depth=1)
-    elif opponent_selection == play_against_hard:
-        while True:
-            print('The hard AI can sometimes take a long time to think.')
+    else:
+        if arguments['depth']:
+            print(f'Using an AI with depth {arguments["depth"]}.\n')
+
+        max_time = None
+        while max_time is None:
+            print(f'The AI can sometimes take a long time to think.')
             print('However, you can set a limit to the amount of time it has to think by entering a maximum wait time'
                   ' in seconds. Press enter to play with an unlimited thinking time.')
             max_time_str = input('Please enter an integer number for the number of seconds the AI can think:')
             if not max_time_str:
                 max_time = float('inf')
-                break
             else:
                 if max_time_str.isdigit():
                     max_time = int(max_time_str)
-                    break
                 else:
                     print('Error. You did not enter an integer value. Please try again.')
-        opponent = agents.MinimaxAI(max_depth=2, max_time=max_time)
-    else:
-        opponent = agents.HexPlayer()
+
+        max_depth = arguments['depth'] if arguments['depth'] else 2
+        opponent = agents.MinimaxAI(max_depth=max_depth, max_time=max_time)
 
     white = 'White'
     black = 'Black'
