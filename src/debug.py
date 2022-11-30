c7c1f0b4a267e4287fa74cca551441b8718a4e75
William Reames <william.reames.19@cnu.edu>
2022-11-30 15:46:30 -0500

    Cleaned up the debug file

--- src/debug.py
+++ src/debug.py
@@ -1,8 +1,6 @@
 """
-William Reames
-CNU Computer Science Capstone
-AI for the Hive Board Game
-2022
+This file is only used for debugging purposes and custom interactions with the game board and the AI.
+This is not meant to be used other than for development purposes.
 """
 import copy
 from timeit import default_timer as timer
@@ -14,254 +12,6 @@ import src.game.spaces as spaces
 import src.agents as agents
 
 import matplotlib.pyplot as plt
-
-
-# def play_game():
-#     # Initialize the game game_board
-#     game_board = board.HiveGameBoard()
-#
-#     while game_board.determine_winner() is None:
-#         print('-' * 50)
-#         print(game_board)
-#         game_board.print_board()
-#
-#         if game_board.is_white_turn():
-#             current_player = 'white'
-#         else:
-#             current_player = 'black'
-#         print('Game turn: {}\nPlayer to Move: {}\nPlayer Turn: {}\n'.format(game_board.turn_number, current_player,
-#                                                                             (game_board.turn_number + 1) // 2))
-#
-#         pieces_to_play, locations_to_place, possible_moves = game_board.get_all_possible_actions()
-#
-#         print('{} has the following pieces to play: {}'.format(current_player, pieces_to_play))
-#         print('{} has the following locations to place a piece: {}'.format(current_player, locations_to_place))
-#         print('{} has the following pieces to move: {}'.format(current_player, set(possible_moves.keys())))
-#
-#         try:
-#             place_or_move = input('Do you want to place a piece (1) or move a piece (2)?')
-#             if int(place_or_move) == 1:
-#                 piece_input = input('Which piece would you like to play?')
-#                 location_input = input('Where would you like to place the piece?')
-#                 location_input = location_input.split(',')
-#                 x_val = int(location_input[0][1:])
-#                 y_val = int(location_input[1][:-1])
-#
-#                 if (x_val, y_val) in locations_to_place:
-#                     print('placing {} at ({}, {})'.format(piece_input, x_val, y_val))
-#                     game_board.place_piece(piece_input, (x_val, y_val))
-#                 else:
-#                     print('Invalid location. Please select from the list of available locations to place a piece')
-#             else:
-#                 piece_loc_input = input('Which piece would you like to move (enter location)?')
-#                 piece_loc_input = piece_loc_input.split(',')
-#                 x_val = int(piece_loc_input[0][1:])
-#                 y_val = int(piece_loc_input[1][:-1])
-#                 if (x_val, y_val) in possible_moves:
-#                     print('{} has the following possible moves: {}'.format((x_val, y_val),
-#                                                                            possible_moves[(x_val, y_val)]))
-#                     move_location_input = input('Where would you like to move the piece?')
-#                     move_location_input = move_location_input.split(',')
-#                     x_val2 = int(move_location_input[0][1:])
-#                     y_val2 = int(move_location_input[1][:-1])
-#
-#                     game_board.move_piece((x_val, y_val), (x_val2, y_val2))
-#                 else:
-#                     print('Invalid selection. Please select from the list of available pieces to move')
-#
-#         # except KeyError:
-#         #     print('Error in processing input: ', traceback.format_exc())
-#         except ValueError:
-#             print('Invalid input: ', traceback.format_exc())
-#
-#     print('Winner: {}'.format(game_board.determine_winner()))
-
-
-def play_game_with_manager(print_out=True, max_actions=5000):
-    board_manager = board.BoardManager()
-
-    actions_performed = []
-    while board_manager.get_board().determine_winner() is None and board_manager.get_board().turn_number < max_actions:
-        current_board = board_manager.get_board()
-        if print_out:
-            current_board.print_board()
-            print(current_board)
-            print('-' * 25)
-            print(f'Current player: {"White" if current_board.is_white_turn() else "Black"}')
-            print(f'White Pieces that can move: {set(current_board.white_possible_moves.keys())}')
-            print(f'Black Pieces that can move: {set(current_board.black_possible_moves.keys())}')
-            print(f'Evaluation: {current_board.evaluate_state()}')
-            print(f'Turn Number: {current_board.turn_number}')
-            print('=' * 50)
-        actions = board_manager.get_action_list()
-        rand_index = random.randint(0, len(actions) - 1)
-        actions_performed.append(actions[rand_index])
-        if print_out or True:
-            print(f'{actions[rand_index]},')
-        board_manager.perform_action(actions[rand_index])
-
-    board_manager.get_board().print_board()
-    if print_out:
-        print(actions_performed)
-    print(f'Total number of actions: {board_manager.get_board().turn_number}')
-
-
-def test_successive_states():
-    board_manager = board.BoardManager()
-
-    current_state = board_manager.get_board()
-    current_state.print_board()
-    for action in current_state.get_action_list():
-        successive_state = board_manager.get_successor(current_state, action)
-        print(successive_state)
-        successive_state.print_board()
-        for successive_action in successive_state.get_action_list():
-            successive_state2 = board_manager.get_successor(successive_state, successive_action)
-            print(successive_state2)
-            successive_state2.print_board()
-        break
-    current_state.print_board()
-
-
-def test_game1():
-    game_board = board.HiveGameBoard()
-    # game_board.place_piece(spcs.Piece.ANT, (0, 0))
-    # game_board.print_board()
-    # game_board.place_piece(spcs.Piece.QUEEN_BEE, (-1, 0))
-    # game_board.print_board()
-    # game_board.place_piece(spcs.Piece.ANT, (1, 1))
-    # game_board.print_board()
-    # print(game_board)
-    # print(game_board.pieces[(-1, 0)])
-    # print(game_board.pieces[(0, 0)])
-    # game_board.move_piece((-1, 0), (0, 1))
-    # game_board.print_board()
-    # print(game_board)
-    #
-    # print('-' * 50)
-    # print(game_board.pieces[(0, 1)])
-
-    PLACE = board.HiveGameBoard.PLACE_PIECE
-    MOVE = board.HiveGameBoard.MOVE_PIECE
-
-    game_board.perform_action_helper(PLACE, (0, 0), piece_type=spaces.Piece.BEETLE)
-    print(game_board)
-    game_board.print_board()
-    game_board.print_hex_board()
-
-    game_board.perform_action_helper(PLACE, (-1, -1), piece_type=spaces.Piece.ANT)
-    print(game_board)
-    game_board.print_board()
-    game_board.print_hex_board()
-
-    # game_board.perform_action_helper(MOVE, (0, 0), new_location=(-1, -1))
-
-    print(game_board)
-    game_board.print_hex_board()
-    # print(board.HiveGameBoard().empty_spaces[(0, 0)])
-    # print(board.HiveGameBoard().empty_spaces[(-1, 0)])
-
-
-def test_game2():
-    game_board = board.HiveGameBoard()
-    PLACE = board.HiveGameBoard.PLACE_PIECE
-    MOVE = board.HiveGameBoard.MOVE_PIECE
-
-    game_board.perform_action_helper(PLACE, (0, 0), piece_type=spaces.Piece.ANT)
-    game_board.print_board()
-    game_board.perform_action_helper(PLACE, (0, 1), piece_type=spaces.Piece.BEETLE)
-    game_board.print_board()
-    game_board.perform_action_helper(PLACE, (0, -1), piece_type=spaces.Piece.QUEEN_BEE)
-    game_board.print_board()
-    game_board.perform_action_helper(MOVE, (0, 1), new_location=(0, 0))
-    game_board.print_board()
-    game_board.perform_action_helper(MOVE, (0, -1), new_location=(-1, -1))
-    game_board.print_board()
-    game_board.perform_action_helper(MOVE, (0, 0), new_location=(-1, -1))
-    game_board.print_board()
-    game_board.perform_action_helper(PLACE, (0, 1), piece_type=spaces.Piece.ANT)
-    game_board.print_board()
-    game_board.perform_action_helper(PLACE, (-2, -2), piece_type=spaces.Piece.QUEEN_BEE)
-    game_board.print_board()
-    game_board.perform_action_helper(PLACE, (0, 2), piece_type=spaces.Piece.GRASSHOPPER)
-    game_board.print_board()
-    game_board.perform_action_helper(PLACE, (-1, -2), piece_type=spaces.Piece.ANT)
-    game_board.print_board()
-    game_board.perform_action_helper(PLACE, (0, 3), piece_type=spaces.Piece.GRASSHOPPER)
-    game_board.print_board()
-    game_board.perform_action_helper(PLACE, (-2, -1), piece_type=spaces.Piece.BEETLE)
-    game_board.print_board()
-    game_board.perform_action_helper(PLACE, (0, 4), piece_type=spaces.Piece.ANT)
-    game_board.print_board()
-    game_board.perform_action_helper(MOVE, (-2, -1), new_location=(-1, -1))
-    game_board.print_board()
-    game_board.perform_action_helper(MOVE, (0, 2), new_location=(0, -1))
-    game_board.print_board()
-
-    print(game_board)
-    print(game_board.pieces[(0, -1)])
-    print(game_board.pieces[(0, 3)])
-
-
-def test_game3():
-    game_board = board.HiveGameBoard()
-    PLACE = board.HiveGameBoard.PLACE_PIECE
-    MOVE = board.HiveGameBoard.MOVE_PIECE
-
-    game_board.perform_action_helper(PLACE, (0, 0), piece_type=spaces.Piece.GRASSHOPPER)
-    game_board.print_board()
-    print('-' * 10)
-    game_board.perform_action_helper(PLACE, (1, 0), piece_type=spaces.Piece.GRASSHOPPER)
-    game_board.print_board()
-    print('-' * 10)
-    game_board.perform_action_helper(PLACE, (-1, 0), piece_type=spaces.Piece.ANT)
-    game_board.print_board()
-    print('-' * 10)
-    game_board.perform_action_helper(MOVE, (1, 0), new_location=(-2, 0))
-    game_board.print_board()
-    print('-' * 10)
-
-    print(game_board)
-    # print(game_board.pieces[(0, 0)])
-    # print(game_board.pieces[(1, 0)])
-    # print(game_board.pieces[(-1, 0)])
-
-
-def test_game4():
-    game_board = board.HiveGameBoard()
-    PLACE = board.HiveGameBoard.PLACE_PIECE
-    MOVE = board.HiveGameBoard.MOVE_PIECE
-
-    game_board.perform_action_helper(PLACE, (0, 0), piece_type=spaces.Piece.BEETLE)
-    game_board.print_board()
-    print('-' * 10)
-    game_board.perform_action_helper(PLACE, (1, 0), piece_type=spaces.Piece.GRASSHOPPER)
-    game_board.print_board()
-    print('-' * 10)
-    game_board.perform_action_helper(PLACE, (-1, -1), piece_type=spaces.Piece.SPIDER)
-    game_board.print_board()
-    print('-' * 10)
-    print(game_board)
-    game_board.perform_action_helper(PLACE, (2, 0), piece_type=spaces.Piece.SPIDER)
-    game_board.print_board()
-    print('-' * 10)
-    print(game_board)
-    game_board.perform_action_helper(MOVE, (-1, -1), new_location=(2, -1))
-    print(game_board)
-    game_board.print_board()
-    print('-' * 10)
-    game_board.perform_action_helper(PLACE, (3, 1), piece_type=spaces.Piece.GRASSHOPPER)
-    print(game_board)
-    game_board.print_board()
-    print('-' * 10)
-    game_board.perform_action_helper(PLACE, (-1, 0), piece_type=spaces.Piece.QUEEN_BEE)
-    print(game_board)
-    game_board.print_board()
-    print('-' * 10)
-    game_board.perform_action_helper(MOVE, (3, 1), new_location=(1, -1))
-    print(game_board)
-    game_board.print_board()
-    print('-' * 10)
 
 
 def test_sliding_rules():
@@ -334,26 +84,6 @@ def test_sliding_rules():
     return game_board
 
 
-def test_game5():
-    game_board = test_sliding_rules()
-    game_board.perform_action_helper(board.HiveGameBoard.PLACE_PIECE, (-3, 0), piece_type=spaces.Piece.ANT)
-    print(game_board)
-    game_board.print_board()
-    print('-' * 10)
-    game_board.perform_action_helper(board.HiveGameBoard.MOVE_PIECE, (2, 0), new_location=(1, 0))
-    print(game_board)
-    game_board.print_board()
-    print('-' * 10)
-    game_board.perform_action_helper(board.HiveGameBoard.MOVE_PIECE, (-3, 0), new_location=(-1, -3))
-    print(game_board)
-    game_board.print_board()
-    print('-' * 10)
-    game_board.perform_action_helper(board.HiveGameBoard.PLACE_PIECE, (2, 0), piece_type=spaces.Piece.ANT)
-    print(game_board)
-    game_board.print_board()
-    print('-' * 10)
-
-
 def demo_game():
     move = board.HiveGameBoard.MOVE_PIECE
     place = board.HiveGameBoard.PLACE_PIECE
@@ -411,7 +141,7 @@ def check_for_errors(word_to_find=None, words_to_ignore=None, max_num_runs=25, m
     while num_runs < max_num_runs:
         try:
             board.BoardManager(new_manager=True)
-            play_game_with_manager(print_out=False, max_actions=max_actions)
+            play_game(random_ai, random_ai, max_turns=max_actions)
         except Exception:
             err_output = traceback.format_exc()
             if word_to_find is None and words_to_ignore is None:
@@ -618,18 +348,10 @@ def test_undo():
     board_state2.print_board(hex_board=False)
 
 
-def test_load_last_game():
+def load_game(filename):
     manager = board.BoardManager(new_manager=True)
-    manager.load_state('didnt_see_win_b4_last_move.hv')
+    manager.load_state(filename)
     manager.get_board().print_board(hex_board=False)
-
-    # manager.perform_action((board.HiveGameBoard.SKIP_TURN, None, None))
-    # manager.perform_action((board.HiveGameBoard.MOVE_PIECE, (0, -1), (0, 0)))
-    # manager.perform_action((board.HiveGameBoard.SKIP_TURN, None, None))
-    #
-    # manager.save_state('didnt_see_win_b4_last_move.hv')
-    #
-    # exit(0)
 
 
 def make_sample_game():
@@ -659,18 +381,11 @@ def make_sample_game():
 
 
 if __name__ == '__main__':
-    make_sample_game()
-    # test_load_last_game()
-    # exit(0)
-    # test_sliding_rules()
+    # make_sample_game()
+    load_game('spider_error_game2.hv')
     # demo_game()
-    # play_game_with_manager()
-    # test_successive_states()
-    # test_game5()
-    # check_for_errors(max_num_runs=250, max_actions=1000)
     # test_undo()
-    # test_game1()
-    # exit(0)
+    # check_for_errors(max_num_runs=250, max_actions=1000)
 
     player = agents.Player()
     hex_player = agents.HexPlayer()
@@ -697,9 +412,10 @@ if __name__ == '__main__':
     actions_processed = []
 
     for i in range(num_games):
+        # Run a game with specified players/AIs for white and black
         _, white_times, black_times, num_actions_per_turn, total_num_actions = play_game(
-            random_ai,
             minimax_ai2,
+            random_ai,
             graph_data_after_run=False,
             # max_turns=50,
         )
